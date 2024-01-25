b78c223f3de7cc670f7c7e22a75e92cba379bd1f
PRADEEPKARLA <156448331+PRADEEPKARLA@users.noreply.github.com>
2024-01-25 19:12:02 +0530

    Add files via upload

---
+++ task_6.js
@@ -0,0 +1,109 @@
+// conditional statements
+//if-else statemments
+let num1=10;
+if (num1==10){
+    console.log("this is correct statement");
+}
+else{
+    console.log("this is incorrect statement");
+}
+
+
+let num2=10;
+if (num2==5){
+    console.log("this is correct statement");
+}
+else{
+    console.log("this is incorrect statement");
+}
+
+let num3=25;
+if (num3==24){
+    console.log("this is correct statement");
+}
+else{
+    console.log("this is incorrect statement");
+}
+
+//if-else if-else
+let num4=10;
+let num5=20;
+if (num4==20){
+    console.log("this num4 is true");
+}
+else if (num5==20){
+    console.log("this num5 is true");
+}
+else{
+    console.log("this is wrong");
+}
+
+
+let time=0;
+if(time<12)
+{
+    console.log("good morning");
+}
+else if(time<=15)
+{
+    console.log("good afternoon");
+}
+else if(time<=18)
+{
+    console.log("good evening");
+}
+else if(time<=24)
+{
+    console.log("good night");
+}
+else 
+{
+    console.log("th input entered is invalid");
+}
+
+//loops
+let num6;
+for(num6=0;num6<=10;num6++)
+{
+    console.log(num6);
+}
+
+let num7=0;
+for(num7=0;num7<=10;num7++)
+ {
+    console.log(num7);
+}
+
+let num8;
+for(num8=10;num8<=0;num8--)
+{
+    console.log(num8);
+}
+let num9;
+for(num9=15;num9<=5;num9--)
+{
+    console.log(num8);
+}
+
+//while loop
+
+let num=0;
+while(num<10)
+{
+    console.log("hello world");
+    num++;
+}
+
+let number=10;
+while(number<=8)
+{
+    console.log("hello world");
+    number--;
+}
+
+let number1=5;
+while(number1<100)
+{
+    console.log("hello world");
+    number1++;
+}
